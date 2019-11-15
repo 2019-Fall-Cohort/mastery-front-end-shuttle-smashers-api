5fa948824e2d829aeaab2cfec84a2e3143a8cb68
Brian Bross <brian.bross@gmail.com>
2019-11-15 00:52:32 -0500

    show players and show teams buttons

--- mastery-front-end-shuttle-smashers/src/App.js
+++ mastery-front-end-shuttle-smashers/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
 
+import Title from './title/title.component';
+
 function App() {
+
+  function ShowTeams()  {
+    console.log("show teams clicked")
+  }
+
+  function ShowPlayers() {
+    console.log("show players clicked")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Why, hello there!
-        </p>
-        
-      </header>
+      <Title text="Shuttle Smashers!"/>
+      <button className="teams-button" onClick={ShowTeams}>TEAMS</button>
+      <button className="players-button" onClick={ShowPlayers}>Players</button>
     </div>
   );
 }
